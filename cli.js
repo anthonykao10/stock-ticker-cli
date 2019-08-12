bb31cc4bab4d36134a1d438299cfa636550f1fb2
anthony <anthonykao10@gmail.com>
2019-08-12 13:04:00 -0700

    update help message, clean up input handling

--- cli.js
+++ cli.js
@@ -9,12 +9,7 @@ const cmd = args[0];
 // HANDLE INPUT ARGS...
 if (!cmd || cmd === '-help' || cmd === 'help' || cmd === '-h') {
   // Display help on explicit cmd (note: strings w/o '-' may conflict with a stock symbol).
-  // show help screen
   stocks.showHelp();
 } else {
-
-  // DETERMINE IF VALID STOCK TICKER--
   stocks.getStockQuote(cmd);
-
-  // DETERMINE TIME (IF AFTERHOURS THEN DON"T SEND REQ)--
 }
